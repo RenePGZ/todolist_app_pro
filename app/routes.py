a88d8d760e5b11b8356dceae3447b682ac924246
Rene Peñaloza Guzmán <rguzman@RPGs-MBA.local>
2025-06-15 13:36:42 -0600

    Migraci+on a PostgreSQL con configuración vía .env

--- app/routes.py
+++ app/routes.py
@@ -107,3 +107,13 @@ def delete_task(id):
     db.session.commit()
     flash("Tarea eliminada.", "info")
     return redirect(url_for("tasks"))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("errors/500.html"), 500
+
+
--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 from app import app
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga variables de .env
+
+# Configura Flask
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
